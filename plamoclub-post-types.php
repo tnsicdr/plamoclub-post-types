6dff693ea29fb386fcb0eba8f13b46feb48bedb5
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2021-09-22 00:19:17 -0400

    Cleaned unused types, added garage kits

--- plamoclub-post-types.php
+++ plamoclub-post-types.php
@@ -3,12 +3,12 @@
  * Plugin Name: Plamo.club Post Types
  * Plugin URI: https://www.github.com/tnsicdr/plamoclub-wp-data
  * Description: Custom Post Types for plamo.club
- * Version: 0.0.2
+ * Version: 0.0.3
  * Requires at least: 5.8
  * Requires PHP: 7.0
  * Author: Thanh Nguyen
  * Author URI: https://www.github.com/tnsicdr
- * Text Domain: plmc-data
+ * Text Domain: plamoclub-post-types
  */
 
  // Exit if accessed directly.
@@ -36,19 +36,18 @@ function register_post_type_plmc_kit() {
                 'parent_item_colon' => __('Parent Model Kit:', 'textdomain'),
                 'all_items' => __('All Model Kits', 'textdomain'),
                 'archives' => __('Model Kit Archives', 'textdomain'),
-                'attributes' => __('Kit Attributes', 'textdomain'),
-                'insert_into_item' => __('Insert into kit', 'textdomain'),
-                'uploaded_to_this_item' => __('Uploaded to this kit', 'textdomain'),
-                'filter_items_list' => __('Filter kits list', 'textdomain'),
+                'attributes' => __('Model Kit Attributes', 'textdomain'),
+                'insert_into_item' => __('Insert into model kit', 'textdomain'),
+                'uploaded_to_this_item' => __('Uploaded to this model kit', 'textdomain'),
+                'filter_items_list' => __('Filter model kits list', 'textdomain'),
                 'items_list_navigation' => __('Model kits list navigation', 'textdomain'),
                 'item_published' => __('Model Kit published', 'textdomain'),
                 'item_published_privately' => __('Model Kit published privately', 'textdomain'),
                 'item_reverted_to_draft' => __('Model kit reverted to draft', 'textdomain'),
                 'item_scheduled' => __('Model Kit scheduled', 'textdomain'),
                 'item_updated' => __('Model Kit updated', 'textdomain'),
-                'item_link' => __('Kit link', 'textdomain'),
+                'item_link' => __('Model Kit link', 'textdomain'),
                 'item_link_description' => __('A link to a model kit', 'textdomain')
-    
             ),
             'supports' => array(
                 'title'
@@ -61,18 +60,48 @@ function register_post_type_plmc_kit() {
             'rewrite' => array( 'slug' => 'model-kit'),
         ),
     );
-}
 
-add_action('graphql_register_types', function () {
-    register_graphql_field('Model Kit', 'manufacturer', [
-        'type' => 'String',
-        'description' => __('The manufacturer of the kit', 'wp-graphql'),
-        'resolve' => function( $post ) {
-            $manufacturer = get_post_meta( $post->ID, 'manufacturer', true );
-            return ! empty( $manufacturer)  ? $manufacturer : '';
-        }
-    ]);
-});
+    register_post_type('plmc_garage_kit',
+        array(
+            'labels' => array(
+                'name' => __('Garage Kits', 'textdomain'),
+                'singular_name' => __('Garage Kit', 'textdomain'),
+                'add_new_item' => __('Add Garage Kit', 'textdomain'),
+                'edit_item' => __('Edit Garage Kit', 'textdomain'),
+                'new_item' => __('New Garage Kit', 'textdomain'),
+                'view_item' => __('View Garage Kit', 'textdomain'),
+                'view_items' => __('View Garage Kits', 'textdomain'),
+                'search_items' => __('Search Garage Kits', 'textdomain'),
+                'not_found' => __('No garage kits found', 'textdomain'),
+                'new_item' => __('No garage kits found in Trash', 'textdomain'),
+                'parent_item_colon' => __('Parent Garage Kit:', 'textdomain'),
+                'all_items' => __('All Garage Kits', 'textdomain'),
+                'archives' => __('Garage Kit Archives', 'textdomain'),
+                'attributes' => __('Garage Kit Attributes', 'textdomain'),
+                'insert_into_item' => __('Insert into garage kit', 'textdomain'),
+                'uploaded_to_this_item' => __('Uploaded to this garage kit', 'textdomain'),
+                'filter_items_list' => __('Filter garage kits list', 'textdomain'),
+                'items_list_navigation' => __('Garage kits list navigation', 'textdomain'),
+                'item_published' => __('Garage Kit published', 'textdomain'),
+                'item_published_privately' => __('Garage Kit published privately', 'textdomain'),
+                'item_reverted_to_draft' => __('Garage kit reverted to draft', 'textdomain'),
+                'item_scheduled' => __('Garage Kit scheduled', 'textdomain'),
+                'item_updated' => __('Garage Kit updated', 'textdomain'),
+                'item_link' => __('Garage Kit link', 'textdomain'),
+                'item_link_description' => __('A link to a garage kit', 'textdomain')
+            ),
+            'supports' => array(
+                'title'
+            ),
+            'public' => true,
+            'show_in_rest' => true,
+            'show_in_graphql' => true,
+            'graphql_single_name' => 'garageKit',
+            'graphql_plural_name' => 'garageKits',
+            'rewrite' => array( 'slug' => 'garage-kit'),
+        ),
+    );
+}
 
 register_activation_hook( __FILE__, 'rewrite_flush_plmc_kit' );
 function rewrite_flush_plmc_kit() {
